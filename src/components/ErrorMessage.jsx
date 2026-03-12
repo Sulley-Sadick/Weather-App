5efbcf47c6a444ac4d1eb0d871373ccb8c505486
Sulley-Sadick <sulleysadick766@gmail.com>
2026-03-12 14:35:34 +0000

    Refractored code

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -11,7 +11,7 @@ export function ErrorMessage({ message, onRetry }) {
   };
 
   return (
-    <div className="flex-center h-40 w-full flex-col rounded-md p-4 shadow-md sm:w-100 dark:bg-gray-800">
+    <div className="flex-center h-40 w-full flex-col rounded-md p-4 shadow-md sm:w-[50%] dark:bg-gray-800">
       <h1 className="text-[1rem] font-semibold text-red-500 dark:text-red-400">
         {message}
       </h1>
@@ -20,7 +20,7 @@ export function ErrorMessage({ message, onRetry }) {
         role="search"
         onClick={handleRetry}
         aria-label="search weather climate"
-        className="mx-auto mt-7 block cursor-pointer rounded-md bg-gray-200 p-2 font-bold text-gray-800 transition-colors duration-400 hover:bg-gray-500 dark:bg-gray-700 dark:text-gray-100"
+        className="mx-auto mt-7 mb-5 block cursor-pointer rounded-md bg-gray-200 p-1 font-bold text-gray-800 transition-colors duration-400 hover:bg-gray-500 dark:bg-gray-700 dark:text-gray-100"
       >
         Try again
       </button>
