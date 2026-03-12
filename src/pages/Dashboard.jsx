f5a5f16b2dd0f788e94dfb4b55074fe8eee51479
Sulley-Sadick <sulleysadick766@gmail.com>
2026-03-12 16:24:59 +0000

    Revert "Split three hour interval weather conditions into HourlyForecast.jsx"
    This reverts commit 6202bdcf56e7893fc3a52b8525bb89c461515e43.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,6 @@ import { useWeatherContext } from "../context/WeatherContext";
 import { ToggleTheme } from "../components/ToggleTheme";
 import { LanguageSwitcher } from "../components/LanguageSwitcher";
 import { useLanguageContext } from "../context/LanguageContext";
-import HourlyForecast from "../components/HourlyForecast";
 
 export function Dashboard() {
   const { selectedWeather } = useWeatherContext();
@@ -45,8 +44,23 @@ export function Dashboard() {
             <LanguageSwitcher />
           </div>
         </div>
-        <HourlyForecast />
-
+        <div className="flex-center flex-col">
+          <h1 className="mb-2 text-3xl font-bold">
+            {t("location.city", { city: selectedWeather.current.name })}
+          </h1>
+          <p>
+            {t("similarLabels.chanceOfRain")}:{" "}
+            {Math.round(selectedWeather.forecast.list[0].pop * 100)}%
+          </p>
+          <img
+            className="mb-4 w-full"
+            src={`https://openweathermap.org/img/wn/${selectedWeather.current.weather[0].icon}@2x.png`}
+            alt={selectedWeather.current.weather[0].main}
+          />
+          <h3 className="mb-10 text-4xl font-black">
+            {Math.round(selectedWeather.current.main.temp)}℃
+          </h3>
+        </div>
         <div className="w-full rounded-md bg-gray-300 p-4 shadow-md md:w-[80%] dark:bg-gray-800">
           <h3 className="my-4 font-bold max-sm:text-center sm:text-left">
             {t("dashboard.titles.todaysWeather")}
